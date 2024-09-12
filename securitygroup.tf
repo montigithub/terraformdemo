94cba97cfcdba9d448b730d1fcfa70cd57350cbe
montigithub <cjacs1608@gmail.com>
2024-09-12 14:02:48 +0530

    add security group code

---
+++ securitygroup.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+}  
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "11.22.33.44/32"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "11.22.33.44/32"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv5" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4       = "11.22.33.44/32"
+  from_port         = 9090
+  ip_protocol       = "tcp"
+  to_port           = 9090
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
